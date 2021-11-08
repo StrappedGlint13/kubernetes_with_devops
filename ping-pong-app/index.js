f245ba37a11fe380f0e4de98bf7f2646c71202bf
StrappedGlint13 <matiasbrax@gmail.com>
2021-11-08 14:56:50 +0200

    ping-pong-app created and deployed using ingress, 1.09

--- ping-pong-app/index.js
+++ ping-pong-app/index.js
@@ -1,12 +1,25 @@
-const express = require('express')
-const app = express()
-
-app.get('/', (req, res) => {
-  res.send('<h1>Hello Kubernetes</h1>')
-})
+const fs = require('fs');
+const Koa = require('koa')
+const app = new Koa()
 require('dotenv').config()
-console.log('lol')
+
+let counter = 0
+
+app.use(async ctx => {
+  if (ctx.path.includes('favicon.ico')) return
+  counter += 1
+  fs.appendFile("./counter.txt", counter.toString(), (err) => {
+    if(err) {
+        return console.log(err);
+    }
+    console.log(counter)
+  }); 
+
+  ctx.body = `Pong: ${counter}`
+});
+
 const PORT = process.env.PORT
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
--- ping-pong-app/logs.md
+++
@@ -1,1 +0,0 @@
-<img src="https://github.com/StrappedGlint13/kubernetes_with_devops/blob/master/images/1.02.png" width="300">
