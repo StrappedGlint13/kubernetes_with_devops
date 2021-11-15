3f447efaa9a968136dbc8b4c54ffb7e91f0e8b3d
StrappedGlint13 <matiasbrax@gmail.com>
2021-11-15 11:19:37 +0200

    todos-service working in cluster and locally

---
+++ todos-service/src/todos-backend.js
@@ -0,0 +1,47 @@
+const { ApolloServer, gql } = require('apollo-server')
+require('dotenv').config()
+
+let todos = [
+  { name: 'TODO 1' }, 
+  { name: 'TODO 2' }
+]
+
+const typeDefs = gql`
+  type Todo {
+    name: String!
+  }
+
+  type Query {
+    allTodos: [Todo!]!
+  }
+
+  type Mutation {
+    addTodo(
+      name: String!
+    ): Todo
+  }
+`
+const resolvers = {
+  Query: {
+    allTodos: async () => todos,
+  },
+  Mutation: {
+    addTodo: (root, args) => {
+      console.log(args)
+      const todo =  { ...args }
+      console.log(todo)
+      todos = todos.concat(todo)
+      console.log(todos)
+      return todo
+    }
+  }
+}
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+})
+
+server.listen().then(({ url }) => {
+  console.log(`Server ready at ${url}`)
+})
