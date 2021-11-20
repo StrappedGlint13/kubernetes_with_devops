5b249e64347acea34eb3ec4b223613cf16cf7599
StrappedGlint13 <matiasbrax@gmail.com>
2021-11-20 08:02:31 +0200

    postgres added with secrets to pingpong counter, 2.7

--- ping-pong-app/index.js
+++ ping-pong-app/index.js
@@ -1,4 +1,4 @@
-const fs = require('fs');
+const { Pool } = require("pg");
 const Koa = require('koa')
 const app = new Koa()
 require('dotenv').config()
@@ -15,10 +15,28 @@ app.use(serve('/usr/src/app/files'))
 app.use(bodyparser())
 app.use(views('./templates', { map: { html: 'nunjucks' }}))
 
+const POSTGRES_PASSWORD = process.env.POSTGRES_PASSWORD
+
+const credentials = {
+  user: "postgres",
+  host: "postgres-service",
+  database: "postgres",
+  password: POSTGRES_PASSWORD,
+  port: 5432,
+};
+
+const pool = new Pool(credentials)
 let counter = 0
+
 
 router.get('/pingpong', async (ctx) => {
   counter += 1
+  pool.query(
+    `INSERT INTO ppa.pongs(counter)VALUES(${counter})`,
+    (err, res) => {
+      console.log(err, res)
+    }
+  )
   return ctx.render('./index', {
     counter: counter
   })
@@ -33,6 +51,22 @@ const PORT = process.env.PORT
 
 app.use(router.routes())
 
-app.listen(PORT, () => {
+
+
+app.listen(PORT, async () => {
+  pool.connect((err, client, release) => {
+    if (err) {
+      return console.error('Error acquiring client', err.stack)
+    }
+    client.query('SELECT NOW()', (err, result) => {
+      release()
+      if (err) {
+        return console.error('Error executing query', err.stack)
+      }
+      console.log(result.rows)
+    })
+  })
   console.log(`Server running on port ${PORT}`)
 })
+
+
